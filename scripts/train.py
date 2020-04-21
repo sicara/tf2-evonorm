cf4ac5fd1123d4764ec6be0d96f5306710fcd4b7
Raphael Meudec <raphaelm@sicara.com>
2020-04-21 09:03:59 +0200

    Switch to click to launch training

--- scripts/train.py
+++ scripts/train.py
@@ -3,12 +3,14 @@ Defines ResNet architectures and train on Cifar-10
 
 Block/Stack decomposition is heavily inspired from official TF/Keras implementation of ResNet
 """
+import pickle
+from pathlib import Path
+
+import click
 import tensorflow as tf
+from loguru import logger
 
 from scripts.resnet import ResnetBuilder, BATCH_NORM_NAME, EVONORM_S0_NAME, EVONORM_B0_NAME
-
-
-INPUT_SHAPE = (32, 32, 3)
 
 
 kwargs = {
@@ -18,21 +20,21 @@ kwargs = {
     "models": tf.keras.models,
 }
 
-
-if __name__ == "__main__":
-    dataset_name = "CIFAR10"
-    is_dataset_cifar_10 = dataset_name == "CIFAR10"
-
-    dataset = tf.keras.datasets.cifar10 if is_dataset_cifar_10 else tf.keras.datasets.cifar100
+@click.command()
+@click.option("--dataset_name", default="cifar10", help="tf.keras dataset to use")
+@click.option("--number_of_experiments", default=1, help="Number of experiments to run")
+@click.option("--logdir", default="logs", help="Logs directory")
+def launch_training(dataset_name, number_of_experiments, logdir):
+    dataset = getattr(tf.keras.datasets, dataset_name)
     (x_train, y_train), (x_test, y_test) = dataset.load_data()
-    num_classes = 10 if is_dataset_cifar_10 else 100
-
 
     x_train = x_train.astype("float32") / 255.
     x_test = x_test.astype("float32") / 255.
-    y_train = tf.keras.utils.to_categorical(y_train, num_classes=num_classes)
-    y_test = tf.keras.utils.to_categorical(y_test, num_classes=num_classes)
+    y_train = tf.keras.utils.to_categorical(y_train)
+    y_test = tf.keras.utils.to_categorical(y_test)
 
+    input_shape = x_train.shape[1:]
+    num_classes = y_train.shape[1]
     batch_size = 64
     epochs = 45
 
@@ -42,39 +44,57 @@ if __name__ == "__main__":
         zoom_range=0.1,
     )
 
-    evonorm_b0_model = ResnetBuilder.build_resnet_18(INPUT_SHAPE, num_classes, block_fn_name=EVONORM_B0_NAME)
-    evonorm_b0_model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    resnet_bn_base_path = Path(f"{logdir}/{dataset_name}/resnet")
+    resnet_evonorm_b0_base_path = Path(f"{logdir}/{dataset_name}/resnet_evonorm_b0")
+    resnet_evonorm_s0_base_path = Path(f"{logdir}/{dataset_name}/resnet_evonorm_s0")
 
-    evonorm_b0_model.fit(
-        data_generator.flow(x_train, y_train, batch_size=batch_size),
-        steps_per_epoch=len(x_train) // batch_size,
-        validation_data=(x_test, y_test),
-        epochs=epochs,
-        callbacks=[tf.keras.callbacks.TensorBoard(f"logs/{dataset_name}/resnet_evonorm_b0")],
-        shuffle=True,
-    )
+    for experiment_index in range(number_of_experiments):
+        resnet_bn_experiment_path = resnet_bn_base_path / str(experiment_index)
+        resnet_evonorm_b0_experiment_path = resnet_evonorm_b0_base_path / str(experiment_index)
+        resnet_evonorm_s0_experiment_path = resnet_evonorm_s0_base_path / str(experiment_index)
 
-    evonorm_model = ResnetBuilder.build_resnet_18(INPUT_SHAPE, num_classes, block_fn_name=EVONORM_S0_NAME)
-    evonorm_model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+        evonorm_b0_model = ResnetBuilder.build_resnet_18(input_shape, num_classes, block_fn_name=EVONORM_B0_NAME)
+        evonorm_b0_model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-    evonorm_model.fit(
-        data_generator.flow(x_train, y_train, batch_size=batch_size),
-        steps_per_epoch=len(x_train) // batch_size,
-        validation_data=(x_test, y_test),
-        epochs=epochs,
-        callbacks=[tf.keras.callbacks.TensorBoard(f"logs/{dataset_name}/resnet_evonorm_s0")],
-        shuffle=True,
-    )
+        history = evonorm_b0_model.fit(
+            data_generator.flow(x_train, y_train, batch_size=batch_size),
+            steps_per_epoch=1, #len(x_train) // batch_size,
+            validation_data=(x_test, y_test),
+            epochs=epochs,
+            callbacks=[tf.keras.callbacks.TensorBoard(resnet_evonorm_b0_experiment_path)],
+            shuffle=True,
+        )
+        with open(resnet_bn_experiment_path / "history", 'wb') as file_pi:
+            pickle.dump(history.history, file_pi)
 
-    model = ResnetBuilder.build_resnet_18(INPUT_SHAPE, num_classes, block_fn_name=BATCH_NORM_NAME)
-    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+        evonorm_model = ResnetBuilder.build_resnet_18(input_shape, num_classes, block_fn_name=EVONORM_S0_NAME)
+        evonorm_model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
-    model.fit(
-        data_generator.flow(x_train, y_train, batch_size=batch_size),
-        steps_per_epoch=len(x_train) // batch_size,
-        validation_data=(x_test, y_test),
-        epochs=epochs,
-        callbacks=[tf.keras.callbacks.TensorBoard(f"logs/{dataset_name}/resnet")],
-        shuffle=True,
-    )
+        evonorm_model.fit(
+            data_generator.flow(x_train, y_train, batch_size=batch_size),
+            steps_per_epoch=1, #len(x_train) // batch_size,
+            validation_data=(x_test, y_test),
+            epochs=epochs,
+            callbacks=[tf.keras.callbacks.TensorBoard(resnet_evonorm_s0_experiment_path)],
+            shuffle=True,
+        )
+        with open(resnet_evonorm_s0_experiment_path / "history", 'wb') as file_pi:
+            pickle.dump(history.history, file_pi)
 
+        model = ResnetBuilder.build_resnet_18(input_shape, num_classes, block_fn_name=BATCH_NORM_NAME)
+        model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+        model.fit(
+            data_generator.flow(x_train, y_train, batch_size=batch_size),
+            steps_per_epoch=1, #len(x_train) // batch_size,
+            validation_data=(x_test, y_test),
+            epochs=epochs,
+            callbacks=[tf.keras.callbacks.TensorBoard(resnet_bn_experiment_path)],
+            shuffle=True,
+        )
+        with open(resnet_evonorm_b0_experiment_path / "history", 'wb') as file_pi:
+            pickle.dump(history.history, file_pi)
+
+
+if __name__ == "__main__":
+    launch_training()
